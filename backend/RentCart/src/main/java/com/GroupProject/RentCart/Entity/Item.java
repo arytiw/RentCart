847c3dda9db53bbf9655e5e65929704503042b6f
Mayurkotwal <mayurkotwal75@gmail.com>
2025-06-24 16:23:22 +0530

    Add new API for orders

---
+++ backend/RentCart/src/main/java/com/GroupProject/RentCart/Entity/Item.java
@@ -0,0 +1,55 @@
+package com.GroupProject.RentCart.Entity;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "items")
+public class Item {
+    @Id
+    private String id;
+    private String title;
+    private String description;
+    private double price;
+    private String userId; // Owner
+
+       // Getters & Setters
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+}
