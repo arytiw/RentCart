910f2839cca99287c52d6123939506dc02339de1
Mayurkotwal <mayurkotwal75@gmail.com>
2025-07-16 23:27:47 +0530

    Local Axios

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,55 +1,16 @@
 import { useState } from 'react';
 import './App.css'
-import Home from './pages/Home';
+import Home from './pages/Home.jsx';
 import ReviewTest from './components/ReviewTest';
 
 import { Container, Button, Row, Col, Card, Navbar, Nav } from 'react-bootstrap';
 
-
-function App() {
-  const [currentPage, setCurrentPage] = useState('home');
-
-  const renderPage = () => {
-    switch (currentPage) {
-      case 'home':
-        return <Home />;
-      case 'review-test':
-        return <ReviewTest />;
-      default:
-        return <Home />;
-    }
-  };
-
-  return (
+function App(){
+  return(
     <>
-      <Navbar bg="dark" variant="dark" expand="lg" className="mb-3">
-        <Container>
-          <Navbar.Brand href="#home">RentCart</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link 
-                href="#home" 
-                onClick={() => setCurrentPage('home')}
-                className={currentPage === 'home' ? 'active' : ''}
-              >
-                Home
-              </Nav.Link>
-              <Nav.Link 
-                href="#review-test" 
-                onClick={() => setCurrentPage('review-test')}
-                className={currentPage === 'review-test' ? 'active' : ''}
-              >
-                Review Test
-              </Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-      
-      {renderPage()}
+      <Home></Home>
+      <ReviewTest></ReviewTest>
     </>
   );
 }
-
 export default App;
